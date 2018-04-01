7f23e82ad67390f2126f7e7e43a35fd15490918b
Bruno Bandeira <brunoban.dev@gmail.com>
2018-04-01 17:30:18 -0300

    update learn test go

---
+++ testes/tabela/strings_test.go
@@ -0,0 +1,31 @@
+package strings
+
+import (
+	"strings"
+	"testing"
+)
+
+const msgIndex = "%s (parte: %s) - indices: esperado (%d) <> encontrado (%d)"
+
+func TestIndex(t *testing.T) {
+	t.Parallel()
+	testes := []struct {
+		texto    string
+		parte    string
+		esperado int
+	}{
+		{"BuRN eh show", "BuRN", 0},
+		{"", "", 0},
+		{"Opa", "opa", -1},
+		{"bruno", "u", 2},
+	}
+
+	for _, teste := range testes {
+		t.Logf("Massa: %v", teste)
+		atual := strings.Index(teste.texto, teste.parte)
+
+		if atual != teste.esperado {
+			t.Errorf(msgIndex, teste.texto, teste.parte, teste.esperado, atual)
+		}
+	}
+}
